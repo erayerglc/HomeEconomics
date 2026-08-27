88916ec7527ee681f0529336dfae96f922cacef3
erayerglc <eray.ergulec@hotmail.com>
2026-08-27 12:07:47 +0300

    feat: update Supabase secret key

--- supabase.js
+++ supabase.js
@@ -4,7 +4,9 @@ const { createClient } = require('@supabase/supabase-js');
 const supabaseUrl = "https://wflecsrbpbbtkdxktufx.supabase.co";
 //const supabaseKey = process.env.SUPABASE_KEY;
 //const supabaseKey = "sb_publishable_OE9SuQq5998hIkROUAbIMQ_R4KvfYwE";
-const supabaseKey = "sb_secret_kfqULWj999dD3Rd4VVAisQ_jMMSNFFq";
+//const supabaseKey = "sb_secret_kfqULWj999dD3Rd4VVAisQ_jMMSNFFq";
+const supabaseKey = "sb_secret_8oPZb9jYo91RndAGKVmvww_rN2ZAY2I";
+
 
 
 let supabase = null;
